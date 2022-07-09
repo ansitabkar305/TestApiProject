ead3b81301a8c94b876e297cf1034c0c1e7eb295
nikhat5311 <nikhat5311@gmail.com>
2022-07-09 13:47:09 +0530

    Add project files.

---
+++ MVCDotNetCore/Controllers/HomeDateController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDotNetCore.Controllers
+{
+    public class HomeDateController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ TestApiProject/Controllers/PageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TestApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PageController : ControllerBase
+    {
+        [HttpGet]
+        public string Say()
+        {
+            return Convert.ToString(DateTime.Now);
+        }
+    }
+}
